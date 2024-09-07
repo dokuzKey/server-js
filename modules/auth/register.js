e9edbf5eb52b11eb336de7896b2af02e9a62a013
musti <musti@duck.com>
2024-09-07 09:35:40 +0300

    Enchantments to the registeration module

--- modules/auth/register.js
+++ modules/auth/register.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const User = require('../../models/User.js');
+const Password = require('../../models/Password.js');
+require('dotenv').config();
 
 const register = async (req, res) => {
     const { username, email, password } = req.query;
@@ -10,12 +12,19 @@ const register = async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
         const token = await bcrypt.hash(hashedPassword, 10);
+        const newPassword = new Password({
+            siteAdress: process.env.SITEADRESS,
+            username,
+            password
+        });
         const newUser = new User({
             username,
             email,
             password: hashedPassword,
             token
         });
+        await newPassword.save();
+        newUser.passwords.push(newPassword);
         await newUser.save();
         return res.status(200).json({status: 1, "token" : token });  
     } catch (error) {
